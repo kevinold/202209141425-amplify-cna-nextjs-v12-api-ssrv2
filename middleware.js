503c9ae75f55ee7d71c6e210b53d06955a31f5d8
Kevin Old <kevin@kevinold.com>
2022-10-03 13:28:44 -0500

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,10 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  if (request.nextUrl.pathname === '/isr-redirect') {
+  if (request.nextUrl.pathname === '/test-isr-redirect') {
     return NextResponse.redirect(new URL('/isr', request.url))
   }
-  if (request.nextUrl.pathname === '/home') {
+  if (request.nextUrl.pathname === '/take-me-home') {
     return NextResponse.rewrite(new URL('/', request.url))
   }
   return NextResponse.next()
